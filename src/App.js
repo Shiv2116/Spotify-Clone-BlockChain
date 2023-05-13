de4fdfe00b546a126057a87671f766630b0c8e29
Shiv2116 <114937365+Shiv2116@users.noreply.github.com>
2023-05-13 18:35:25 +0530

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Routes, Route ,Link} from "react-router-dom";
+import Home from "./pages/Home";
+import Album from './pages/Album';
+import './App.css';
+import { Layout, Space } from 'antd';
+import Spotify from "./images/Spotify.png";
+import {SearchOutlined,DownCircleOutlined} from "@ant-design/icons"
+
+const {  Footer, Sider, Content } = Layout;
+
+
+const App = () => {
+  return(
+  <Layout>
+        <Sider style={siderStyle}><img src={Spotify} alt= "logo" className="logo" />
+       <div className='searchBar'>
+        <span>Search</span>
+        <SearchOutlined className="searchLogo" style={{fontSize:"20px" ,}} />
+        </div>
+        <Link to="/">
+          <p style={{color: "#1DB954"}}>Home</p>
+          
+        </Link>
+        <p>Your Music</p>
+        <div className='recentPlayed'>
+          <p className='recentTitle'>Recently Played</p>
+          <div className='install'>
+            <span>Install App</span>
+            <DownCircleOutlined className="installLogo"style={{fontSize:"20px"}} />
+          </div>
+        </div>
+        </Sider>
+    <Content className='contentWindow' style={contentStyle}><Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/album" element={<Album />} />
+      </Routes>
+     </Content>
+    </Layout>
+    
+  )
+};
+
+export default App;
+
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '40px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+};
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '40px',
+
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+};
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+};
